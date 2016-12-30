8cf5b20e0d373600b51fc93acb32e1c809f1ee57
carloscalderon <cdcalderon@gmail.com>
2016-12-30 04:40:35 -0600

    Initial Commit

---
+++ CharlieCakepops/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CharlieCakepops
+//
+//  Created by carlos calderon on 12/30/16.
+//  Copyright © 2016 carlos calderon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
